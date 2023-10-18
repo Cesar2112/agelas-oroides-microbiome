8a3c0bee3b0d023961f8d24f862e89484dc98bc1
Ruiz César <cesar.ruiz@imbe.fr>
2023-10-18 15:56:31 +0000

    Update ScriptAoroidesMic.R until Alpha_Div

--- Data pre-treatment/ScriptAoroidesMic.R
+++ Data pre-treatment/ScriptAoroidesMic.R
@@ -1,9 +1,9 @@
 # R packages needed
 # "phyloseq" to merge ASV_table, ASV_counttable, SampleData, Phylotree 
-# "xtable" 
+# "knitr" for table creation  
 # "phangorn" for phylogenetic trees
 # "decipher" performs alignement of multiple sequences following a guided tree
-# "microbiome"
+# "microbiome" series of OTU/ASV operations and analysis
 # "eulerr"
 # "ggplot2"
 # "vegan" 
@@ -27,9 +27,6 @@ ASV = otu_table(ASV_16S1_Table, taxa_are_rows = TRUE)
 TAX = tax_table(ASV_Tax_16S1)
 sampledata = sample_data(Mapfile)
 
-#Creation object type Phyloseq
-physeq16s1Ori = phyloseq(ASV, TAX, sampledata)
-
 # Creation od phylogenetic tree
 
 seqs <- getSequences(TAX)
@@ -44,13 +41,74 @@ fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
                     rearrangement = "stochastic", control = pml.control(trace = 0))
 detach("package:phangorn", unload=TRUE)
 
-physeq16s1=merge_phyloseq(physeq16s1Ori, phy_tree(fitGTR$tree))
-"> physeq16s1
+#Creation object type Phyloseq
+physeq16s1Ori = phyloseq(ASV, TAX, sampledata, phy_tree(fitGTR$tree))
+"
 phyloseq-class experiment-level object
-otu_table()   OTU Table:         [ 446 taxa and 78 samples ]
+otu_table()   OTU Table:         [ 446 taxa and 30 samples ]
 sample_data() Sample Data:       [ 78 samples by 6 sample variables ]
 tax_table()   Taxonomy Table:    [ 446 taxa by 7 taxonomic ranks ]
 phy_tree()    Phylogenetic Tree: [ 446 tips and 444 internal nodes ]"
+
+#Prune taxa not present in any sample (if they exist)
+physeq16s1NoZeroTaxa <- prune_taxa(taxa_sums(physeq16s1) > 0, physeq16s1)
+physeq16s1<-physeq16s1NoZeroTaxa
+"phyloseq-class experiment-level object
+otu_table()   OTU Table:         [ 365 taxa and 30 samples ]"
+#Create a table with reads number per sample:
+nbReads<- sample_sums(physeq16s1)
+write.table(nbReads, "nbReads_161S1_MS.tsv", sep = "\t", quote=F, col.names=NA)
+
+
+#Taxa filtering by prevalecence
+#Prevalecence Agelas
+prevdf_AO = apply(X = otu_table(physeq16s1),
+                  MARGIN = ifelse(taxa_are_rows(physeq16s1), yes = 1, no = 2),
+                  FUN = function(x){sum(x > 0)})
+prevdf_AO = data.frame(Prevalence = prevdf_AO,
+                       TotalAbundance = taxa_sums(physeq16s1),
+                       tax_table(physeq16s1))
+plyr::ddply(prevdf_AO, "Phylum", function(df1){cbind(mean(df1$Prevalence),
+                                                     sum(df1$Prevalence))}) -> dfprev_AO
+kable(dfprev_AO)
+write.table(dfprev_AO, "PhylumReadsPrev_161S1_AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+filterPhyla = c("Bacteroidetes", "Campilobacterota", "Chlamydiae", "Deinococcus-Thermus", "Rhodothermaeota") 
+(physeq16FIN = subset_taxa(physeq16s1, !Phylum %in% filterPhyla))
+# physeq16FIN [ 351 taxa and 30 samples ]
+
+#Change Taxa names par ASV#
+taxa_names(Physeq_MS)
+taxa_names(Physeq_MS) <- paste0("ASV", seq(ntaxa(physeq16FIN)))
+
+#Overview of taxonomic assignement
+TaxOverviwAO <-table(tax_table(physeq16FIN)[, 2])
+write.table(TaxOverviwAO, "TaxOverview_AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+
+# Create table, number of features for each phyla and class
+PhylaTable_AO<-table(tax_table(physeq16FIN)[, "Phylum"], exclude = NULL)
+write.table(PhylaTable_AO, "PhylaTable16s1AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+ClassTable_AO<-table(tax_table(physeq16FIN)[, "Class"], exclude = NULL)
+write.table(ClassTable_AO, "ClassTable16s1AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+OrderTable_AO<-table(tax_table(physeq16FIN)[, "Order"], exclude = NULL)
+write.table(OrderTable_AO, "OrderTable16s1AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+FamTable_AO<-table(tax_table(physeq16FIN)[, "Family"], exclude = NULL)
+write.table(FamTable_AO, "FamilyTable16s1AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+GenTable_AO<-table(tax_table(physeq16FIN)[, "Genus"], exclude = NULL)
+write.table(GenTable_AO, "GenusTable16s1AO_FN.tsv", sep = "\t", quote=F, col.names=NA)
+
+##Find out the number of a taxon, for example:
+Ao <- as.vector(tax_table(physeq16FIN)[, "Phylum"])
+b_Ao <- unique(Ao)#10 Phyla
+Ao <- as.vector(tax_table(physeq16FIN)[, "Class"])
+b_Ao <- unique(Ao)#18 Class
+Ao <- as.vector(tax_table(physeq16FIN)[, "Order"])
+b_Ao <- unique(Ao)#31 Order
+
+# convert to relative abundance (for abundance barplots)
+physeq16FIN_AbRel = transform_sample_counts(physeq16FIN, function(x) x / sum(x) )
+
+               **************** DIVERSITE ALPHA*********************
+set.seed(12345)
 
 
 
